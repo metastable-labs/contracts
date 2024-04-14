49c9e6c748c89dcf6d77498e4b9db27e46822b14
Njoku Emmanuel <kalunjoku123@gmail.com>
2024-04-14 11:06:38 -0600

    chore: add BaseMigrate contracts

---
+++ script/deploy-migration.s.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "../src/migration/BasedMigrateERC20.sol";
+import "../src/migration/factory/BasedERC20Factory.sol";
+import {Script} from "forge-std/Script.sol";
+
+contract DeployMigrationScript is Script {
+    function run() external returns (address implementation, address factory) {
+        // get pvt key from env file, log associated address
+        uint256 privateKey = vm.envUint("PRIVATE_KEY");
+        address L2BridgeAddress = vm.envAddress("L2_BRIDGE_ADDRESS");
+
+        vm.startBroadcast(privateKey);
+        // deploy implementation
+        implementation = address(new BasedMigrateERC20());
+
+        // deploy factory
+        factory = address(
+            new BasedERC20Factory(address(implementation), L2BridgeAddress)
+        );
+
+        vm.stopBroadcast();
+
+        return (implementation, factory);
+    }
+}
